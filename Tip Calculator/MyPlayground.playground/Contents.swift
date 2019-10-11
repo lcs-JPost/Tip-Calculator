88c15415e5a9034eafb321c2273d3600fc24ab17
Justus Post <JPost@lcs.on.ca>
2019-10-10 22:33:31 -0400

    Did the view

---
+++ Tip Calculator/MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+let AmountOfBill = 120.34
+let tipPercentageString = "15"
+let tipPercentageDouble = Double(tipPercentageString)
+let tipPercentage = 0.15
+let tip = AmountOfBill * tipPercentage
+
