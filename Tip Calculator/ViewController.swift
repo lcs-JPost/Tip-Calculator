9b067303b81eb74ee9d95221433f3d7d1fd377e3
Justus Post <JPost@lcs.on.ca>
2019-10-17 19:34:06 -0400

    Defined Every needed Constant, performed the Calculations, created the labels, don't know what to do to make it work

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -12,24 +12,56 @@ class ViewController: UIViewController {
 
     // MARK: Properties
     
-    @IBOutlet weak var AmountOfBill: UITextField!
+    @IBOutlet weak var SubmittedAmountOfBill: UITextField!
     
     
-    @IBOutlet weak var AmountOfPeople: UITextField!
+    @IBOutlet weak var SubmittedAmountOfPeople: UITextField!
     
     
-    @IBOutlet weak var TipInPercent: UITextField!
+    @IBOutlet weak var SubmittedTipInPercent: UITextField!
     
+    @IBOutlet weak var TotalTipLabel: UILabel!
+    
+    @IBOutlet weak var TipPerPersonLabel: UILabel!
+    
+    
+
+    // Mark: Initializers
+    
+  
     
     // MARK: Methods
    
-    
+ 
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     
+    }
     
     
+    @IBAction func CalculateTheTip(_ sender: Any) {
+        
+        let BillText = SubmittedAmountOfBill.text!
+        
+        let Bill = Double(BillText)!
+        
+        let TipInPercentText = SubmittedTipInPercent.text!
+        
+        let TipInPercent = Double(TipInPercentText)!
+        
+        let TotalTip = Bill/(TipInPercent/100)
+        
+        let AmountOfPeopleText = SubmittedAmountOfPeople.text!
+        
+        let AmountOfPeople = Double(AmountOfPeopleText)!
+        
+        let TipPerPerson = TotalTip/AmountOfPeople
+        
+        TotalTipLabel.text = String(TotalTip)
+        
+        TipPerPersonLabel.text = String(TipPerPerson)
     }
     
     
